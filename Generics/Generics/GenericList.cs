f60e496fb22b46913e23d5f366fb52a3e8f4b6fd
Chaudhary <anoop.chaudhary@thermofisher.com>
2017-09-05 12:39:36 +0530

    Generic List with add() and showList() elements implemented

---
+++ Generics/Generics/GenericList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class GenericList<T>
+    {
+
+        private class Node
+        {
+
+            private T data;
+            public T Data
+            {
+                get { return data; }
+                set { data = value; }
+            }
+
+            private Node next;
+            public Node Next
+            {
+                get { return next; }
+                set { next = value; }
+            }
+
+
+            public Node(T _data)
+            {
+                next = null;
+                data = _data;
+            }
+        }
+
+        private Node head;
+
+        public GenericList()
+        {
+            head = null;
+        }
+
+        public void AddData(T _data)
+        {
+            Node node = new Node(_data);
+            node.Next = head;
+            head = node;
+        }
+
+        //public T this[int Index] { get; set; }
+
+        public void showList()
+        {
+            Node currentNode = head;
+            while (currentNode != null)
+            {
+                Console.Write("{0} ", currentNode.Data);
+                currentNode = currentNode.Next;
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Generics
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GenericList<int> integerList = new GenericList<int>();
+            GenericList<string> stringList = new GenericList<string>();
+            integerList.AddData(1);
+            integerList.AddData(2);
+            integerList.AddData(3);
+            stringList.AddData("Ajay");
+            stringList.AddData("M.");
+            integerList.showList();
+            stringList.showList();
+            Console.ReadKey();
+        }
+    }
+}
