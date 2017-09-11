90520f948c6b0bb94280f8ef8c1b982091285573
Chaudhary <anoop.chaudhary@thermofisher.com>
2017-09-11 15:12:32 +0530

    Threading -start\abort\sleep\join done

---
+++ ThreadSampleConsoleApp/ThreadSampleConsoleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+
+namespace ThreadingSampleApp
+{
+    public class Alpha
+    {
+        public void Beta()
+        {
+            while (true)
+            {
+                Console.WriteLine("Alph.Beta is running in its own thread.");
+            }
+        }
+    }
+
+    public class Simple
+    {
+        public static int Main()
+        {
+            Console.WriteLine("Thread Start/Stop/Join Sample");
+            Alpha alpha = new Alpha();
+            //Use ThreadStart delegate to pass the method in Thread constructor
+            Thread thread = new Thread(new ThreadStart(alpha.Beta));
+            thread.Start();
+
+            while (!thread.IsAlive) ;
+            Thread.Sleep(1);
+            try
+            {
+                thread.Abort();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Unable to abort!" + ex.Message);
+            }
+
+            thread.Join();
+            Console.WriteLine();
+            Console.WriteLine("Alpha.Beta has finished");
+            try
+            {
+                Console.WriteLine("Try to restart the Alpha.Beta thread");
+                thread.Start();
+            }
+            catch (ThreadStateException )
+            {
+
+                Console.WriteLine(">>>>ThreadStateException trying to restart Alpha.Beta. " );
+                Console.WriteLine("Expected since aborted threads cannot be restarted.<<<<<");
+            }
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
